ac2c05c098061bc2c06c03d0864ddc1bbab004e5
Rave Guy <Rave Guy>
2020-05-16 04:08:53 +0300

    fix12

--- modules/chezar/src/chezar.cpp
+++ modules/chezar/src/chezar.cpp
@@ -2,20 +2,17 @@
 
 #include "include/chezar.h"
 
-char* Encrypt (char* msg, char key) {
+char* Encrypt(char* msg, char key) {
     char ch;
     for (int i = 0; msg[i] != '\0'; ++i) {
         ch = msg[i];
-        //encrypt for lowercase letter
         if (ch >= 'a' && ch <= 'z') {
            ch = ch + key;
            if (ch > 'z') {
               ch = ch - 'z' + 'a' - 1;
            }
            msg[i] = ch;
-       }
-        //encrypt for uppercase letter
-        else if (ch >= 'A' && ch <= 'Z') {
+       } else if (ch >= 'A' && ch <= 'Z') {
            ch = ch + key;
            if (ch > 'Z') {
               ch = ch - 'Z' + 'A' - 1;
@@ -26,20 +23,17 @@ char* Encrypt (char* msg, char key) {
     return msg;
 }
 
-char* Decrypt (char* msg, char key) {
+char* Decrypt(char* msg, char key) {
     char ch;
     for (int i = 0; msg[i] != '\0'; ++i) {
        ch = msg[i];
-       //decrypt for lowercase letter
        if (ch >= 'a' && ch <= 'z') {
            ch = ch - key;
-           if(ch < 'a'){
+           if (ch < 'a') {
                 ch = ch + 'z' - 'a' + 1;
             }
             msg[i] = ch;
-        }
-       //decrypt for uppercase letter
-       else if (ch >= 'A' && ch <= 'Z') {
+        } else if (ch >= 'A' && ch <= 'Z') {
           ch = ch - key;
           if (ch < 'A') {
              ch = ch + 'Z' - 'A' + 1;
