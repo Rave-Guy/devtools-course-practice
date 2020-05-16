111f46b236d6d3b94553be7a0271415183728445
Rave Guy <Rave Guy>
2020-05-16 05:11:16 +0300

    fix

--- modules/chezar/test/test_chezar.cpp
+++ modules/chezar/test/test_chezar.cpp
@@ -5,31 +5,16 @@
 #include "include/chezar.h"
 
 TEST(Suchkov_Makar_ChezarTest, Encrypt_Simple) {
-    // Arrange
     char a[] = "abcd";
-
-    // Act
-
-    // Assert
     ASSERT_STRCASEEQ(Encrypt(a, 1), "bcde");
 }
 
 TEST(Suchkov_Makar_ChezarTest, Decrypt_Simple) {
-    // Arrange
     char a[] = "bcde";
-
-    // Act
-
-    // Assert
     ASSERT_STRCASEEQ(Decrypt(a, 1), "abcd");
 }
 
 TEST(Suchkov_Makar_ChezarTest, Decrypt_Encrypt_Simple) {
-    // Arrange
     char a[] = "abcdefghijklmnopqrstuvwxyz";
-    
-    // Act
-
-    // Assert
-    ASSERT_STRCASEEQ(Encrypt(a, 5),  Decrypt(a, 5));
+	ASEEQ(Encrypt(a, 5),  Decrypt(a, 5));
 }
