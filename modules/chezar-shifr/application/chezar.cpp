280f971986fbaf2c21d867ca99441413e29abdb7
Rave Guy <Rave Guy>
2020-05-16 04:35:48 +0300

    fix15

---
+++ modules/chezar-shifr/application/chezar.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 Suchkov Makar
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#include "include/chezar.h"
+
+int main(int argc, const char** argv) {
+    return 0;
+}
