e930243308dc7591f4f36adc0e7d3e9b81086071
Rave Guy <Rave Guy>
2020-05-16 03:54:36 +0300

    fix8

--- modules/chezar/include/chezar.h
+++ modules/chezar/include/chezar.h
@@ -1,9 +1,9 @@
 // Copyright 2020 Suchkov Makar
 
-#ifndef MODULES_CHEZAR_H_
-#define MODULES_CHEZAR_H_
+#ifndef MODULES_CHEZAR_INCLUDE_CHEZAR_H_
+#define MODULES_CHEZAR_INCLUDE_CHEZAR_H_
 
 char* Decrypt(char* msg, char key);
 char* Encrypt(char* msg, char key);
 
-#endif  // MODULES_CHEZAR_H_
+#endif  // MODULES_CHEZAR_INCLUDE_CHEZAR_H_
