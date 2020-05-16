162e9fb225523b1bde61ec4b10aaf20dc4fa0cd3
Rave Guy <34068822+Rave-Guy@users.noreply.github.com>
2020-05-16 20:15:32 +0300

    fix31

--- modules/chezar/test/test_chezar.cpp
+++ modules/chezar/test/test_chezar.cpp
@@ -4,21 +4,6 @@
 
 #include "include/chezar.h"
 
-TEST(Suchkov_Makar_ChezarTest, Encrypt_Simple) {
-    char a[] = "abcd";
-    ASSERT_STRCASEEQ(Encrypt(a, 1), "bcde");
-}
-
-TEST(Suchkov_Makar_ChezarTest, Decrypt_Simple) {
-    char a[] = "bcde";
-    ASSERT_STRCASEEQ(Decrypt(a, 1), "abcd");
-}
-
-TEST(Suchkov_Makar_ChezarTest, Decrypt_Encrypt_Simple) {
-    char a[] = "abcdefghijklmnopqrstuvwxyz";
-    ASSERT_STRCASEEQ(Encrypt(a, 5),  Decrypt(a, 5));
-}
-
 TEST(Suchkov_Makar_ChezarTest, Can_Add_key_Data) {
     // Arrange
     Chezar estr;
