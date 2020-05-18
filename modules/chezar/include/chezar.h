3c8bb345a2e5a6b4fe18b7defade7035df9f5398
Rave Guy <34068822+Rave-Guy@users.noreply.github.com>
2020-05-18 20:35:20 +0300

    init_lab2

---
+++ modules/chezar/include/chezar.h
@@ -0,0 +1,26 @@
+// Copyright 2020 Suchkov Makar
+
+#ifndef MODULES_CHEZAR_INCLUDE_CHEZAR_H_
+#define MODULES_CHEZAR_INCLUDE_CHEZAR_H_
+
+#include <iostream>
+
+class Chezar {
+ public:
+    Chezar();
+    Chezar(char *str, int key);
+    ~Chezar();
+    char* GetStr();
+    bool SetStr(char* str);
+    bool SetKey(int key);
+    int GetKey();
+    bool isEncrypt();
+    bool Decrypt();
+    bool Encrypt();
+ private:
+    char* msg = nullptr;
+    int key = 0;
+    bool is_e = false;
+};
+
+#endif  // MODULES_CHEZAR_INCLUDE_CHEZAR_H_
