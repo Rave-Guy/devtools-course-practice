68eaf0415585e3831176eb60720bc8d820e3d660
Rave Guy <34068822+Rave-Guy@users.noreply.github.com>
2020-05-26 00:31:47 +0300

    fix

--- modules/bitfield/src/bitfield-app.cpp
+++ modules/bitfield/src/bitfield-app.cpp
@@ -61,8 +61,9 @@ std::string Bitfieldapp::operator()(int argc, const char** argv) {
 
     Bitfield bitf(args.bitfield_size);
     std::random_device rd;
-    std::default_random_engine generator(rd);
-    int a = static_cast<int>(generator);
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<int> dist(1, 100);
+    int a = static_cast<int>(dist(mt));
     if (argc == 4) {
         switch (args.type) {
          case 1:
